42232525cbd9617013cc3db6b7ca5d4e4b1ca67d
Patricia Takehara <paty.takeharab@outlook.com>
2022-12-16 16:56:48 -0300

    Commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Desafio
+{
+  static void Main()
+  {
+    int quantidadeEntradas = 3;
+
+    while (quantidadeEntradas-- > 0)
+    {
+      int numeroQuantidade = Int32.Parse(Console.ReadLine());
+      string[] tartarugas = Console.ReadLine().Split(" ");
+      var maiorVelocidade = Int32.Parse(tartarugas[0]);
+
+      for (int i = 0; i < tartarugas.Length; i++)
+      {
+        var tartaruga = Int32.Parse(tartarugas[i]);
+
+        if (tartaruga > maiorVelocidade)
+        {
+          maiorVelocidade = tartaruga;
+        }
+      }
+
+      if (maiorVelocidade < 10)
+      {
+        Console.WriteLine(1);
+      }
+      else if (maiorVelocidade >= 10 && maiorVelocidade < 20)
+      {
+        Console.WriteLine(2);
+      }
+      else
+      {
+        Console.WriteLine(3);
+      }
+    }
+  }
+}
